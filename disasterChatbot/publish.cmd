675b234459131af63914e10afac15adda5b284a1
lubhaniagarwal <44601972+lubhaniagarwal@users.noreply.github.com>
2019-11-25 16:41:08 +0530

    Add files via upload

---
+++ disasterChatbot/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=disasterreliefrishi-b216-Web-Deploy.pubxml -p:Password=AGcc8g9wzAquCmy0F4fr6EERjaHtHPo0eexvcNQkhrQ0w5zjuYEp4TPqP3tu
+
